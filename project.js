340ed6483af7979d7ab15a72486f48c9d4549307
Darius Robotean <robotean@gmail.com>
2025-11-21 00:51:35 +0200

    adding functions for number of lines and bet size

--- project.js
+++ project.js
@@ -1,12 +1,15 @@
 // 1. Deposit some money
-// 2. Colect a bet amount
-// 3. Deteremine number of line to be on
+// 2. Deteremine number of line to be on
+// 3. Colect a bet amount
 // 4. Spin the slot machine
 // 5. Check if the user won
 // 6. give the user their winnings
 // 7. play again
 
 const prompt = require("prompt-sync")();
+const ROWS = 3;
+const COLS = 3;
+
 
 const deposit = () => {
   while (true) {
@@ -21,5 +24,36 @@ const deposit = () => {
   }
 };
 
-const depositAmount = deposit();
-console.log(depositAmount);
+
+const getNumberOfLines = () =>{
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || (numberOfLines <= 0 || numberOfLines > 3)) {
+      console.log("Invalid number of lines, try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+}
+
+const getBet = (balanceParameter, linesParameter) =>{
+while (true) {
+    const bet = prompt("Enter the total bet per line: ");
+    const betAmount = parseFloat(bet);
+
+    if (isNaN(betAmount) || betAmount <= 0 || betAmount > balanceParameter / linesParameter ) {
+      console.log("Invalid bet amount");
+    } else {
+      return betAmount ;
+    }
+  }  
+}
+
+
+let balance = deposit();
+const numberOfChosenLines = getNumberOfLines();
+const bet = getBet(balance ,  numberOfChosenLines);
+console.log(bet)
+ 
