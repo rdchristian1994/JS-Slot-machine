2e256204a85dd104606df60cc414f909ea9ae5b0
Darius Robotean <robotean@gmail.com>
2025-11-22 00:28:28 +0200

    added random reel picking

--- project.js
+++ project.js
@@ -10,6 +10,19 @@ const prompt = require("prompt-sync")();
 const ROWS = 3;
 const COLS = 3;
 
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOL_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
 
 const deposit = () => {
   while (true) {
@@ -24,36 +37,60 @@ const deposit = () => {
   }
 };
 
-
-const getNumberOfLines = () =>{
+const getNumberOfLines = () => {
   while (true) {
     const lines = prompt("Enter the number of lines to bet on (1-3): ");
     const numberOfLines = parseFloat(lines);
 
-    if (isNaN(numberOfLines) || (numberOfLines <= 0 || numberOfLines > 3)) {
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
       console.log("Invalid number of lines, try again");
     } else {
       return numberOfLines;
     }
   }
-}
+};
 
-const getBet = (balanceParameter, linesParameter) =>{
-while (true) {
+const getBet = (balanceParameter, linesParameter) => {
+  while (true) {
     const bet = prompt("Enter the total bet per line: ");
     const betAmount = parseFloat(bet);
 
-    if (isNaN(betAmount) || betAmount <= 0 || betAmount > balanceParameter / linesParameter ) {
+    if (
+      isNaN(betAmount) ||
+      betAmount <= 0 ||
+      betAmount > balanceParameter / linesParameter
+    ) {
       console.log("Invalid bet amount");
     } else {
-      return betAmount ;
+      return betAmount;
     }
-  }  
-}
+  }
+};
 
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+  const reels = [];
+  for (let i = 0; i < COLS; i++) {
+    reels.push([]);
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+  return reels;
+};
 
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfChosenLines = getNumberOfLines();
-const bet = getBet(balance ,  numberOfChosenLines);
-console.log(bet)
- 
+const bet = getBet(balance, numberOfChosenLines);
+console.log(bet);
